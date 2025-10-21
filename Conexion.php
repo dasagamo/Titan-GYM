d6dc0dc36c9e0f0a78c42e74fa4e3473f6c0048d
dasagamo <dasagamo1@gmail.com>
2025-10-20 21:41:09 -0500

    Delete MySQLi connection code
    Remove MySQLi connection code from Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "1212";
-$database = "gymdb";
 
-// Crear conexión MySQLi
-$conexion = mysqli_connect($servername, $username, $password, $database);
-
-// Verificar conexión
-if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
-}
-?>
