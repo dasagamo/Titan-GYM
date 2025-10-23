021a6ea47c9791e0f691c1a82283f5325046997c
dasagamo <dasagamo1@gmail.com>
2025-10-22 21:15:26 -0500

    Add files via upload

--- Gym_offiall/Index_Cliente.php
+++ Gym_offiall/Index_Cliente.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Titan GYM</title>
-  <link rel="stylesheet" href="Desing/Style_Index.css">
+  <link rel="stylesheet" href="Desing/Style_Index.css?v=<?php echo time(); ?>">
+
   <!-- Íconos de redes -->
   <script src="https://kit.fontawesome.com/6c5b55f7a3.js" crossorigin="anonymous"></script>
 </head>
